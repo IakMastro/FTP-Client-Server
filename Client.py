ec0ef5d91c6e3f76f3c1b69f56794b894cae9e46
Iakovos Mastrogiannopoulos <cse242017102@uniwa.gr>
2019-09-02 23:01:32 +0300

    Terminal Commands: ls, exit, help

--- Client.py
+++ Client.py
@@ -13,7 +13,7 @@ except:
 	exit()
 
 username = input("Username: ")
-password = getpass("Password: ")
+password = getpass()
 
 try:
 	ftp.login(username, password)
@@ -22,4 +22,28 @@ except:
 	print(RED + "Wrong username or password" + DEFAULT)
 	exit()
 
-print(BLUE + "Success!" + DEFAULT)
+print(BLUE + "Connection successful!" + DEFAULT)
+
+print("commands: help, cd, ls, download, send, exit")
+while True:
+	answer = input("$")
+	if answer == "exit":
+		break
+
+	elif answer == "help":
+		print("cd: change the directory. Type the path of the new directory")
+		print("ls: print whatever is on the current directory")
+		print("donwload: download a file from the current directory")
+		print("send: upload a file to the current directory")
+		print("exit: exit the program")
+
+	elif answer == "ls":
+		for file in ftp.nlst():
+			print(BLUE + file + DEFAULT)
+
+	else: 
+		whitespace = answer.find(' ')
+		answer = answer.split(" ", 1)
+		print(answer)
+
+print(BLUE + "Logging you out" + DEFAULT)
