4e27d367052489914042717d5e0a59e3381448ee
Iakovos Mastrogiannopoulos <cse242017102@uniwa.gr>
2019-09-02 23:40:33 +0300

    Client: cd Terminal Command

--- Client.py
+++ Client.py
@@ -3,6 +3,7 @@ from getpass import getpass
 
 BLUE = '\033[1;34m'
 RED = '\033[0;31m'
+GREEN = '\033[1;32m'
 DEFAULT = '\033[0m'
 
 ftp = FTP('')
@@ -26,24 +27,31 @@ print(BLUE + "Connection successful!" + DEFAULT)
 
 print("commands: help, cd, ls, download, send, exit")
 while True:
-	answer = input("$")
+	answer = input("$" + GREEN)
+	print(DEFAULT, end='')
 	if answer == "exit":
 		break
 
 	elif answer == "help":
-		print("cd: change the directory. Type the path of the new directory")
-		print("ls: print whatever is on the current directory")
-		print("donwload: download a file from the current directory")
-		print("send: upload a file to the current directory")
-		print("exit: exit the program")
+		print(GREEN + "cd" + DEFAULT + ":\t  change the directory. Type the path of the new directory")
+		print(GREEN + "ls" + DEFAULT + ":\t  print whatever is on the current directory")
+		print(GREEN + "donwload" + DEFAULT + ": download a file from the current directory")
+		print(GREEN + "send" + DEFAULT + ":\t  upload a file to the current directory")
+		print(GREEN + "exit" + DEFAULT + ":\t  exit the program")
 
 	elif answer == "ls":
+		print(f"Current path: {ftp.pwd()}")
 		for file in ftp.nlst():
 			print(BLUE + file + DEFAULT)
 
-	else: 
-		whitespace = answer.find(' ')
-		answer = answer.split(" ", 1)
-		print(answer)
+	else:
+		try: 
+			answer = answer.split(' ', answer.find(' '))
+			print(answer[0], answer[1])
+			if answer[0] == "cd":
+				ftp.cwd(answer[1])
+
+		except:
+			print(RED + "You have to give an argument as well" + DEFAULT)
 
 print(BLUE + "Logging you out" + DEFAULT)
--- Server.py
+++ Server.py
@@ -4,7 +4,7 @@ from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
 authorizer = DummyAuthorizer()
-authorizer.add_user("the doctor", "1234", "/mnt/d/", perm="elradfmw")
+authorizer.add_user("the doctor", "1234", "/mnt/g/", perm="elradfmw")
 authorizer.add_anonymous("/mnt/c/Users/akis/Desktop/Python/FTP Client-Server", perm="r")
 
 handler = FTPHandler
