0c312295b90c1d0a2455bf024ccd95564858d8f7
Iakovos Mastrogiannopoulos <cse242017102@uniwa.gr>
2019-09-02 18:35:19 +0300

    Server script.

---
+++ Server.py
@@ -0,0 +1,13 @@
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+authorizer = DummyAuthorizer()
+authorizer.add_user("the doctor", "1234", "D:\\", perm="elradfmw")
+authorizer.add_anonymous("C:\\Users\\akis\\Desktop\\Python\\FTP Client-Server", perm="r")
+
+handler = FTPHandler
+handler.authorizer = authorizer
+
+server = FTPServer(("192.168.1.9", 2000), handler)
+server.serve_forever()
